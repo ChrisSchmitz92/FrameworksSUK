d6842e02dcd821c8266737316c938f3313073823
Bogdan Krenz (inf852) <bogdan.krenz@smail.th-koeln.de>
2019-05-09 09:25:22 +0200

    weather API and traffic API added but not implemented

---
+++ project/weather.js
@@ -0,0 +1,19 @@
+const request = require("request");
+
+/*Anwendungslogik*/
+
+let apiKey = "99cf7877476de8ea9e1305f2153597ba";
+let city = "Gummersbach";
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+request(url, function(err, response, body) {
+  if (err) {
+    console.log("error:", error);
+  } else {
+    let weather = JSON.parse(body);
+    // let lat = weather.lat;
+    // let long = weather.long;
+    let message = `Es sind ${weather.main.temp}° in ${city}.`;
+    console.log(message);
+  }
+});
